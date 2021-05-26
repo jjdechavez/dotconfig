ddc355f8a1d99b39eb1642174eb1a3d74a67d4f7
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-05-26 10:48:32 +0800

    Add installation for fzf

--- install.sh
+++ install.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 
-# Installing Nerd Font
-echo "Installing nerd fonts"
-echo "Creating folder for fonts at ~/.local/share/fonts"
-mkdir -p ~/.local/share/fonts
-echo "Created folder for fonts at ~/.local/share/fonts"
+echo "$OSTYPE"
 
-echo "Fetching Fira Code Nerd Font"
-cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
-echo "Finished downloading Fira Code Nerd Font"
-echo "Done: installing nerd fonts!"
+# Installing Nerd Font
+installnerdfont() {
+  echo "Work: Installing nerd fonts"
+  echo "Creating folder for fonts at ~/.local/share/fonts"
+  # mkdir -p ~/.local/share/fonts
+  echo "Created folder for fonts at ~/.local/share/fonts"
+
+  echo "Fetching Fira Code Nerd Font"
+  # cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+  echo "Finished downloading Fira Code Nerd Font"
+  echo "Done: Installing nerd fonts!"
+}
+
+# Installing FZF
+installfzf() {
+  echo "Work: Installing FZF"
+  [ -n "$(cat /etc/os-release | grep Ubuntu)" ] && installfzfonubuntu
+  [ -f "/etc/arch-release" ] && installfzfonarch
+  echo "Done: Installing FZF"
+}
+
+installfzfonarch() {
+  echo "Installing fzf on arch"
+  sudo pacman -S fzf
+
+  sudo pacman -S ripgrep
+
+  # yay -S universal-ctags-git
+
+  sudo pacman -S the_silver_searcher
+
+  pacman -S fd
+}
+
+installfzfonubuntu() {
+  echo "Installing fzf on ubuntu"
+  sudo apt install fzf
+
+  sudo apt install ripgrep
+
+  sudo apt install universal-ctags
+
+  sudo apt install silversearcher-ag
+
+  sudo apt install fd-find
+}
+
+setup() {
+  echo "Starting to setup.."
+  installnerdfont
+  installfzf
+  echo "Done"
+}
+
+# Execute setup
+setup
