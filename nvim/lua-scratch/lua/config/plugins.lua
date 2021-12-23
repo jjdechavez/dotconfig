72cb235ce37ea798a401c8285d1a76c32702c8c2
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2021-12-23 21:52:37 +0800

    Added bufferline on lua-scratch

--- nvim/lua-scratch/lua/config/plugins.lua
+++ nvim/lua-scratch/lua/config/plugins.lua
@@ -48,6 +48,8 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
 
   -- Color scheme
   use 'EdenEast/nightfox.nvim'
@@ -68,6 +70,8 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
