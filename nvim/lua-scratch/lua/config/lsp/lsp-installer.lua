48d18e0c8b57baeeebb4758d8e15de3d8561316b
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-03-06 11:02:07 +0800

    Add lsp config on html

--- nvim/lua-scratch/lua/config/lsp/lsp-installer.lua
+++ nvim/lua-scratch/lua/config/lsp/lsp-installer.lua
@@ -21,14 +21,19 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
-	  if server.name == "pyright" then
-	    local pyright_opts = require("config.lsp.settings.pyright")
-	    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	  end
+    if server.name == "pyright" then
+      local pyright_opts = require("config.lsp.settings.pyright")
+      opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+    end
 
 	  if server.name == "tsserver" then
 	    local tsserver_opts = require("config.lsp.settings.tsserver")
 	    opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
+	  end
+
+	  if server.name == "html" then
+	    local html_opts = require("config.lsp.settings.html")
+	    opts = vim.tbl_deep_extend("force", html_opts, opts)
 	  end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
