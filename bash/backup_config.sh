86b6145e5d3a5cfac9220e039cc6fd97b31308db
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-04-07 09:00:20 +0800

    Add backup nvim script

--- bash/backup_config.sh
+++ bash/backup_config.sh
@@ -34,12 +34,12 @@ version() {
 
 push_bashrc_to_github() {
   echo "Preparing to push bashrc on github."
-  my
-  cd dotconfig
+  cd /mnt/ExternalHDD/my/dotconfig/
 
   git add bash/.manjaro-bashrc
   read -p 'Commit message: ' message
   git commit -m "$message"
+  git status
 
   echo "Starting to push on github."
   git push
@@ -75,11 +75,53 @@ backup_bashrc() {
   fi
 }
 
+push_nvim_to_github() {
+  echo "Preparing to push nvim on github."
+  cd /mnt/ExternalHDD/my/dotconfig/
+
+  git add nvim/lua-scratch
+  read -p 'Commit message: ' message
+  git commit -m "$message"
+  git status
+
+  echo "Starting to push on github."
+  git push
+  echo "Succesfully pushed on github."
+  cd $CURRENT_DIR
+}
+
+update_nvim() {
+  echo "Starting to update nvim"
+  cp -r $HOME/.config/nvim /mnt/ExternalHDD/my/dotconfig/nvim/lua-scratch
+  echo "Updated succesfully nvim"
+
+  push_nvim_to_github
+}
+
+backup_nvim() {
+  #  Checkif lua-scratch is exist on dotconfig
+  #   - If exist, remove the old one
+  #     > cp new bashrc from $HOME directory to dotconfig
+  #   - else, cp new bashrc from $HOME directory to dotconfig
+  echo "Running backup_nvim"
+
+  if [[ -d /mnt/ExternalHDD/my/dotconfig/nvim/lua-scratch ]]; then
+    echo "Exist, starting to remove old nvim"
+    rm -rf /mnt/ExternalHDD/my/dotconfig/nvim/lua-scratch
+    echo "Removed succesfully nvim"
+
+    update_nvim
+  else
+    echo "nvim does not exist."
+    update_nvim
+  fi
+}
+
 main () {
   if [[ "$CONFIG" == "bashrc" ]]; then
     backup_bashrc
   elif [[ "$CONFIG" == "nvim" ]]; then
-    echo "Running backup_nvim"
+    backup_nvim
   else 
     echo "Error: Invalid argument"
   fi
