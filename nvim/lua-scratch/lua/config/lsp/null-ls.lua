8033f9a26d80240c4a72b98ddff4a29fc5de9a20
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2022-12-23 11:28:49 +0800

    Add blade formatter on null-ls

--- nvim/lua-scratch/lua/config/lsp/null-ls.lua
+++ nvim/lua-scratch/lua/config/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -16,32 +16,36 @@ null_ls.setup {
     end
   end,
   sources = {
-    --[[ formatting.prettier.with({ ]]
-    --[[   extra_args = { "--single-quote", "--jsx-single-quote" }, ]]
-    --[[   filetypes = { ]]
-    --[[     "javascript", ]]
-    --[[     "javascriptreact", ]]
-    --[[     "typescript", ]]
-    --[[     "typescriptreact", ]]
-    --[[     "vue", ]]
-    --[[     "css", ]]
-    --[[     "scss", ]]
-    --[[     "less", ]]
-    --[[     "html", ]]
-    --[[     "json", ]]
-    --[[     "yaml", ]]
-    --[[     "markdown", ]]
-    --[[     "graphql", ]]
-    --[[     "heex", ]]
-    --[[   }, ]]
-    --[[ }), ]]
-    formatting.eslint,
+    formatting.prettier.with({
+      extra_args = { "--single-quote", "--jsx-single-quote" },
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "yaml",
+        "markdown",
+        "graphql",
+        "heex",
+      },
+    }),
+    --[[ formatting.eslint, ]]
     formatting.mix,
     formatting.elm_format,
     formatting.black.with { extra_args = { "--fast" } },
     formatting.djhtml,
     formatting.gofmt,
-    formatting.prismaFmt
+    formatting.prismaFmt,
+    formatting.blade_formatter.with({
+      extra_args = { "--indent-size=2" },
+      filetypes = { "edge", "blade" }
+    })
     -- formatting.stylua,
   },
 }
