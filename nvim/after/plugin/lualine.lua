48fbe0bf7742dd9cd86e10b11db073ab33e1872d
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-01-22 12:11:25 +0800

    Remove winbar on lualine and custom winbat

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -90,6 +90,22 @@ lualine.setup({
     lualine_y = {},
     lualine_z = {},
   },
+  -- winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = { '%f', '%m' },
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
+  -- inactive_winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = { '%f', '%m' },
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {}
+  -- },
   tabline = {},
   extensions = {},
 })
