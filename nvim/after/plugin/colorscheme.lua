c455836adf495a08cd06e1625bfeb3b2f0b888bf
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-11-22 20:13:29 +0800

    Updated config on colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -45,9 +45,19 @@ function SetColorscheme(color, theme)
     AccentColorscheme()
   end
 
+  if color == "base16-colorscheme" then
+    -- Tomorrow - custom bg or base00
+    require('base16-colorscheme').setup({
+      base00 = '#EBEFF0', base01 = '#e0e0e0', base02 = '#d6d6d6', base03 = '#8e908c',
+      base04 = '#969896', base05 = '#4d4d4c', base06 = '#282a2e', base07 = '#1d1f21',
+      base08 = '#c82829', base09 = '#f5871f', base0A = '#eab700', base0B = '#718c00',
+      base0C = '#3e999f', base0D = '#4271ae', base0E = '#8959a8', base0F = '#a3685a'
+    })
+    return
+  end
+
   vim.cmd.colorscheme(color)
 end
 
-SetColorscheme("zenbones", "light")
--- SetColorscheme("neobones", "dark")
--- SetColorscheme("accent")
+-- SetColorscheme("zenbones", "light")
+SetColorscheme("neobones", "dark")
