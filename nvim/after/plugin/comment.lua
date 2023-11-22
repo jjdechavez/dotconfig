75f5be8dcd5571ef981e2ad8d96991216e15d19b
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-11-22 20:13:15 +0800

    Updated comment.nvim

--- nvim/after/plugin/comment.lua
+++ nvim/after/plugin/comment.lua
@@ -1,3 +1,9 @@
 require('Comment').setup({
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  config = function ()
+  end
 })
+
+-- Filetype custom comment
+local ft = require('Comment.ft')
+ft.set('edge', '{{-- %s --}}')
