aa5bc68fcfa418ce1535a6baa84c33be21fca4fd
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-11-22 20:13:01 +0800

    Comment extra_args: inorder to follow to project setup prettier

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,4 +1,6 @@
 local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+local utils = require("null-ls.utils")
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -14,26 +16,42 @@ null_ls.setup {
   --   end
   -- end,
   sources = {
-    -- formatting.prettier.with({
-    --   extra_args = { "--single-quote", "--jsx-single-quote" },
-    --   filetypes = {
-    --     "javascript",
-    --     "javascriptreact",
-    --     "typescript",
-    --     "typescriptreact",
-    --     "vue",
-    --     "css",
-    --     "scss",
-    --     "less",
-    --     "html",
-    --     "json",
-    --     "yaml",
-    --     "markdown",
-    --     "graphql",
-    --     "heex",
-    --   }
-    -- }),
-    formatting.eslint,
+    formatting.prettier.with({
+      -- extra_args = { "--single-quote", "--jsx-single-quote" },
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "yaml",
+        "markdown",
+        "graphql",
+        "heex",
+      },
+      cwd = helpers.cache.by_bufnr(function(params)
+        return utils.root_pattern(
+        -- https://prettier.io/docs/en/configuration.html
+          ".prettierrc",
+          ".prettierrc.json",
+          ".prettierrc.yml",
+          ".prettierrc.yaml",
+          ".prettierrc.json5",
+          ".prettierrc.js",
+          ".prettierrc.cjs",
+          ".prettier.config.js",
+          ".prettier.config.cjs",
+          ".prettierrc.toml",
+          "package.json"
+        )(params.bufname)
+      end),
+    }),
+    -- formatting.eslint,
     -- formatting.mix,
     formatting.elm_format,
     -- formatting.black.with { extra_args = { "--fast" } },
