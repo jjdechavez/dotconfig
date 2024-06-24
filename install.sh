e19d0e341f7579b23be4fd11a1095663aa48dc80
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2024-06-24 11:42:25 +0800

    Added install for mac

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ install_fzf() {
   # [  ] - meaning test the command inside of the brackets
   [ -n "$(cat /etc/os-release | grep Ubuntu)" ] && install_fzf_on_ubuntu
   [ -f "/etc/arch-release" ] && install_fzf_on_arch
+  [[ $OSTYPE == 'darwin'* ]] && install_fzf_on_mac
   echo "Done: Installing FZF"
 }
 
@@ -50,11 +51,21 @@ install_fzf_on_ubuntu() {
   sudo apt install fd-find
 }
 
+install_fzf_on_mac() {
+  echo "Installing fzf on mac"
+  brew install fzf
+  brew install fd
+  brew ripgrep
+  brew install universal-ctags
+  brew install the_silver_searcher
+}
+
 # Installing tmux
 install_tmux() {
   echo "Work: Installing tmux"
   [ -n "$(cat /etc/os-release | grep Ubuntu)" ] && install_tmux_on_ubuntu
   [ -f "/etc/arch-release" ] && install_tmux_on_arch
+  [[ $OSTYPE == 'darwin'* ]] && install_tmux_on_mac
 
   echo "Work: Copy .tmux.conf"
   cp ./tmux/.tmux.conf $HOME/.tmux.conf
@@ -73,12 +84,19 @@ install_tmux_on_arch() {
   sudo pacman -S xclip
 }
 
+install_tmux_on_mac() {
+  echo "Installing tmux on mac"
+  brew install tmux
+  brew install xclip
+}
+
 # Install Delta - git syntax highlight
 install_delta() {
   echo "Work: Installing delta"
   # TODO: setup for ubuntu
   # [ -n "$(cat /etc/os-release | grep Ubuntu)" ] && install_delta_on_ubuntu
   [ -f "/etc/arch-release" ] && install_delta_on_arch
+  [[ $OSTYPE == 'darwin'* ]] && install_delta_on_mac
 
   echo "Done: Installing delta"
 }
@@ -88,9 +106,15 @@ install_delta_on_arch() {
   sudo pacman -S git-delta
 }
 
+install_delta_on_mac() {
+  echo "Installing delta on mac"
+  brew install git-delta
+}
+
 install_lazygit() {
   echo "Work: Installing lazygit"
   [ -f "/etc/arch-release" ] && install_lazygit_on_arch
+  [[ $OSTYPE == 'darwin'* ]] && install_lazygit_on_mac
 
   echo "Work: Copy lazygit config.yml"
   cp ./lazygit/config.yml $HOME/.config/lazygit/
@@ -102,12 +126,19 @@ install_lazygit_on_arch() {
   sudo pacman -S lazygit
 }
 
+install_lazygit_on_mac() {
+  echo "Installing lazygit on mac"
+  brew install lazygit
+}
+
 setup() {
   echo "Starting to setup.."
-  install_nerdfont
-  install_fzf
-  install_tmux
-  install_delta
+  # install_nerdfont
+  # install_fzf
+  # install_tmux
+  # install_delta
+  # install_lazygit
+  # TODO copy alacrity config
   echo "Done"
 }
 
